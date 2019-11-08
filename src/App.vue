8ff5b973c7fd7b8fd87cd1497d948754737a5942
Alex Scheaua <alexandru-florin.scheaua@stefanini.com>
2019-11-08 14:55:40 +0200

    Version 2.0 - WIP

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <Navbar :changeComponent="changeComponent"/>
+        <main>
+            <div :is="component" :changeComponent="changeComponent"></div>
+        </main>
+    </div>
+</template>
+
+<script>
+
+    import Navbar from "./components/Navbar";
+    import Home from "./components/Home";
+    import About from "./components/About";
+    import Projects from "./components/Projects";
+    import Contact from "./components/Contact";
+
+    export default {
+        name: 'app',
+        components: {
+            Navbar,
+            Home,
+            About,
+            Projects,
+            Contact,
+        },
+        data() {
+            return {
+                component: Home
+            }
+        },
+        methods: {
+            changeComponent(component) {
+                this.component = component;
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import "scss/main";
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,65 @@
+<template>
+    <header>
+        <div @click="toggleMenu" class="menu-btn" :class="showMenu()">
+            <span class="menu-btn__burger" :class="showMenu()"></span>
+        </div>
+        <nav class="nav" :class="showMenu()">
+            <ul class="menu-nav" :class="showMenu()">
+                <li v-for="item in navItems" :key="item.component" class="menu-nav__item" :class="showMenu()">
+                    <a @click="clickHandler(item.component)" :class="showMenu()" class="menu-nav__link active">{{item.text}}</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "navbar",
+        props: [
+          "changeComponent"
+        ],
+        data(){
+          return {
+              isShowMenu: false,
+              navItems: [
+                  {
+                      component: 'Home',
+                      text: 'Home'
+                  },
+                  {
+                      component: 'About',
+                      text: 'About Me'
+                  },
+                  {
+                      component: 'Projects',
+                      text: 'My Projects'
+                  },
+                  {
+                      component: 'Contact',
+                      text: 'Contact'
+                  },
+              ]
+          }
+        },
+        methods: {
+            showMenu(){
+                if(this.isShowMenu) {
+                    return 'open'
+                }
+                return ''
+            },
+            toggleMenu(){
+                this.isShowMenu = !this.isShowMenu;
+            },
+            clickHandler(component){
+                this.changeComponent(component);
+                this.isShowMenu = false;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
