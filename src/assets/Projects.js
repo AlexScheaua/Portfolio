6069964781e7140764b5120ca578f44a431feabc
Alex Scheaua <alexandru-florin.scheaua@stefanini.com>
2019-11-11 10:37:35 +0200

    Version 2.0.1 - minor modifications

--- src/assets/Projects.js
+++ src/assets/Projects.js
@@ -1,19 +1,26 @@
 module.exports = [
     {
+        name: 'My portfolio',
+        description: 'Is a single page app created with VueJs and has a mobile first design created using SCSS/Sass',
+        url: 'https://alex-scheaua.netlify.com',
+        github: 'https://github.com/AlexScheaua/Portfolio',
+        image: require('./portfolio.png')
+    },
+    {
         name: 'VSPEND',
-        description: 'VSPEND is a PWA application created with NodeJs, VueJs, Bootstrap and Firebase. Its purpose is to allow users to share a monthly budget and expenses with someone else.',
+        description: 'Is a mobile first design Progressive Web App application created with NodeJs, VueJs, Bootstrap and Firebase. Its purpose is to allow users to share a monthly budget and expenses with someone else.',
         url: 'https://vspend.herokuapp.com',
         github: 'https://github.com/AlexScheaua/vue-spend',
         image: require('./vspend.png')
     }, {
         name: 'Tangram',
-        description: '',
+        description: 'Is a game created with javascript, NodeJs/expressJS and firebase that uses a RESTful architecture. It is made after the popular chinese puzzle, and has a timed leaderboard for each player',
         url: 'https://tangram-node.herokuapp.com',
         github: 'https://github.com/AlexScheauaStefanini/Tangram',
         image: require('./tangram.png')
     }, {
         name: 'eShop',
-        description: '',
+        description: 'Is a RESTful responsive e-commerce website made with javascript, expressJs and mongoDb with mongoose.',
         url: 'https://alexscheaua.github.io/eShop/',
         github: 'https://github.com/AlexScheaua/eShop',
         image: require('./eshop.png')
